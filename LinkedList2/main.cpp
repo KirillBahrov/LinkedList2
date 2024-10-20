4780b1d4003d2c8d6c15f3c62c38e5620d0628a2
KirillBahrov <kirillbahrow@gmail.com>
2024-10-20 14:45:32 +0300

    Прога

---
+++ LinkedList2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "..//Solver/LinkedList2.h"
+
+int main()
+{
+    RUT::MIIT::LinkedList list = { 1, 2, 3, 4 };
+    std::cout << "Linked list: " << list << std::endl;
+}
