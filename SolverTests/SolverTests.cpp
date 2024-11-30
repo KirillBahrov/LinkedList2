5cd2955ba9fa49ae43dee52e716c0d040a9af68d
KirillBahrov <kirillbahrow@gmail.com>
2024-11-30 21:09:50 +0300

    изменил

--- SolverTests/SolverTests.cpp
+++ SolverTests/SolverTests.cpp
@@ -1,14 +1,15 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../Solver/Linkedlist2.h"
+#include "../TestClass/Point.h"
 #include <assert.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace RUT::MIIT;
-
-namespace LinkedTest
+using namespace rut::miit::pentagon;
+namespace UnitTest1
 {
-    TEST_CLASS(LinkedTest)
+    TEST_CLASS(UnitTest1)
     {
     public:
         TEST_METHOD(DefaultConstructor_Success)
@@ -36,7 +37,7 @@ namespace LinkedTest
             LinkedList<int> list;
             list.pushFront(2);
             list.pushFront(1);
-            Assert::AreEqual(std::string("21"), list.ToString());
+            Assert::AreEqual(std::string("12"), list.ToString());
         }
 
         TEST_METHOD(PopBack_ValidData_Success)
@@ -75,18 +76,6 @@ namespace LinkedTest
             Assert::AreEqual(std::string("123"), list2.ToString());
         }
 
-        TEST_METHOD(PopFrontEmpty_ValidData_Success)
-        {
-            LinkedList<int> list;
-            Assert::ExpectException<std::out_of_range>([&list]() { list.popFront(); });
-        }
-
-        TEST_METHOD(PopBackEmpty_ValidData_Success)
-        {
-            LinkedList<int> list;
-            Assert::ExpectException<std::out_of_range>([&list]() { list.popBack(); });
-        }
-
         TEST_METHOD(Insert_ValidData_Success)
         {
             LinkedList<int> list{ 1, 3 };
@@ -101,12 +90,6 @@ namespace LinkedTest
             Assert::AreEqual(std::string("123"), list.ToString());
         }
 
-        TEST_METHOD(InsertOutOfBounds_ValidData_Success)
-        {
-            LinkedList<int> list{ 1, 2 };
-            Assert::ExpectException<std::out_of_range>([&list]() { list.insert(3, 3); });
-        }
-
         TEST_METHOD(Remove_ValidData_Success)
         {
             LinkedList<int> list{ 1, 2, 3 };
@@ -119,12 +102,6 @@ namespace LinkedTest
             LinkedList<int> list{ 1, 2, 3 };
             list.remove(0);
             Assert::AreEqual(std::string("23"), list.ToString());
-        }
-
-        TEST_METHOD(RemoveOutOfBounds_ValidData_Success)
-        {
-            LinkedList<int> list{ 1, 2, 3 };
-            Assert::ExpectException<std::out_of_range>([&list]() { list.remove(3); });
         }
 
         TEST_METHOD(ComplexOperations_ValidData_Success)
@@ -202,18 +179,6 @@ namespace LinkedTest
             Assert::AreEqual(std::string("HelloWorld"), list2.ToString());
         }
 
-        TEST_METHOD(PopFirstFromEmptyString)
-        {
-            LinkedList<std::string> list;
-            Assert::ExpectException<std::out_of_range>([&list]() { list.popFront(); });
-        }
-
-        TEST_METHOD(PopLastFromEmptyString)
-        {
-            LinkedList<std::string> list;
-            Assert::ExpectException<std::out_of_range>([&list]() { list.popBack(); });
-        }
-
         TEST_METHOD(TestInsertString)
         {
             LinkedList<std::string> list{ "Hello", "World" };
@@ -228,17 +193,108 @@ namespace LinkedTest
             Assert::AreEqual(std::string("HelloWorld"), list.ToString());
         }
 
-        TEST_METHOD(InsertOutOfBoundsString)
-        {
-            LinkedList<std::string> list{ "Hello", "World" };
-            Assert::ExpectException<std::out_of_range>([&list]() { list.insert(3, "OutOfBounds"); });
-        }
-
         TEST_METHOD(TestRemoveString)
         {
             LinkedList<std::string> list{ "Hello", "Beautiful", "World" };
             list.remove(1);
             Assert::AreEqual(std::string("HelloWorld"), list.ToString());
+        }
+
+        TEST_METHOD(DefaultCtorPoint_Success)
+        {
+            LinkedList<Point> list;
+            Assert::IsTrue(list.isEmpty());
+        }
+
+        TEST_METHOD(InitializerListCtorPoint_ValidData_Success)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list{ a, b };
+            Assert::IsTrue(a == list.peekFront() && b == list.peekBack());
+        }
+
+        TEST_METHOD(AddToBackPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list;
+            list.pushBack(a);
+            list.pushBack(b);
+            Assert::IsTrue(a == list.peekFront() && b == list.peekBack());
+        }
+
+        TEST_METHOD(AddToFrontPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list;
+            list.pushFront(b);
+            list.pushFront(a);
+            Assert::IsTrue(a == list.peekFront() && b == list.peekBack());
+        }
+
+        TEST_METHOD(RemoveLastPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list{ a, b };
+            list.popBack();
+            Assert::IsTrue(a == list.peekBack());
+        }
+
+        TEST_METHOD(RemoveFirstPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list{ a, b };
+            list.popFront();
+            Assert::IsTrue(b == list.peekFront());
+        }
+
+        TEST_METHOD(CopyConstructorPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list1{ a, b };
+            LinkedList<Point> list2(list1);
+            Assert::AreEqual(list1.ToString(), list2.ToString());
+        }
+
+        TEST_METHOD(MoveConstructorPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list1original{ a, b };
+            LinkedList<Point> list1(list1original);
+            LinkedList<Point> list2(std::move(list1original));
+            Assert::IsTrue(list1original.isEmpty() && list1.ToString() == list2.ToString());
+        }
+
+        TEST_METHOD(AssignmentOperatorPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list1{ a, b };
+            LinkedList<Point> list2;
+            list2 = list1;
+            Assert::AreEqual(list1.ToString(), list2.ToString());
+        }
+
+        TEST_METHOD(TestInsertPoint)
+        {
+            Point a(0, 0), b(1, 1), c(2, 2);
+            LinkedList<Point> list{ a, b }, listexp{ a,c,b };
+            list.insert(1, c);
+            Assert::AreEqual(listexp.ToString(), list.ToString());
+        }
+
+        TEST_METHOD(InsertAtStartPoint)
+        {
+            Point a(0, 0), b(1, 1);
+            LinkedList<Point> list{ b }, listexp{ a,b };
+            list.insert(0, a);
+            Assert::AreEqual(listexp.ToString(), list.ToString());
+        }
+
+        TEST_METHOD(TestRemovePoint)
+        {
+            Point a(0, 0), b(1, 1), c(2, 2);
+            LinkedList<Point> list{ a,c,b }, listexp{ a,b };
+            list.remove(1);
+            Assert::AreEqual(listexp.ToString(), list.ToString());
         }
     };
 }
